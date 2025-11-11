63d58dc17ed7a4392dce91e903f0c4d321a10912
tripatisumit <99213688+sumittripati@users.noreply.github.com>
2025-11-11 16:51:19 +0530

    first commit

--- backend/mail/templates/passwordUpdate.js
+++ backend/mail/templates/passwordUpdate.js
@@ -66,7 +66,7 @@ exports.passwordUpdated = (email, name) => {
             </div>
             <div class="support">If you have any questions or need further assistance, please feel free to reach out to us
                 at
-                <a href="mailto:info@BajajStudy.com">info@BajajStudy.com</a>. We are here to help!
+                <a href="mailto:info@sumittechStudy.com">info@sumittechStudy.com</a>. We are here to help!
             </div>
         </div>
     </body>
